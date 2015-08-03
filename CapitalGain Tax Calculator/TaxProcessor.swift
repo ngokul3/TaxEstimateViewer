c4684ba976c80fe3a34eb36a2a20e16d18e54b45
Gokul Narasimhan <ngokul3@gmail.com>
2015-08-02 23:19:09 -0400

    Tax Processor logic complete

--- CapitalGain Tax Calculator/TaxProcessor.swift
+++ CapitalGain Tax Calculator/TaxProcessor.swift
@@ -25,11 +25,10 @@ class TaxProcessor
     func GetTaxableIncome(filingStatus: FilingStatus, lstLotTerm: [LotTerm])
     {
         var lstTaxBracket = TaxOnCapitalGainLossUp.GetTaxHairCut(filingStatus)
+ 
+        var shortTermTax = lstTaxBracket.filter({m in m.Term.rawValue == ENumTerm.ShortTerm.rawValue}).first
         
-      //  var shortTermTaxList : [TaxBracket]
-        var shortTermTaxList = lstTaxBracket.filter({m in m.Term.rawValue == ENumTerm.ShortTerm.rawValue})
-        
-        var longTermTaxList = lstTaxBracket.filter({m in m.Term.rawValue == ENumTerm.LongTerm.rawValue})
+        var longTermTax = lstTaxBracket.filter({m in m.Term.rawValue == ENumTerm.LongTerm.rawValue}).first
         
         
         let lotTermMap: [Double] = lstLotTerm.map { return $0.TermRealizedGainLoss }
@@ -37,9 +36,89 @@ class TaxProcessor
         
         let shortTermGL = lstLotTerm.filter({m in m.Term.rawValue == ENumTerm.ShortTerm.rawValue}).map { return $0.TermRealizedGainLoss }.reduce(0) { return $0 + $1 }
         
-      //  let shortTermGL
+        let section1256TermGL = lstLotTerm.filter({m in m.Term.rawValue == ENumTerm.Section1256.rawValue}).map { return $0.TermRealizedGainLoss }.reduce(0) { return $0 + $1 }
         
+        let longTermGLIncluding1256 = longTermGL + (section1256TermGL * 0.60)
+        let shortTermGLIncluding1256 = shortTermGL + (section1256TermGL * 0.40)
+        
+        if(longTermGLIncluding1256 >= 0 && shortTermGLIncluding1256 >= 0)
+        {
+            filingStatus.TaxOnLTCapitalGain = GetCapitalGainTax(longTermGL, termTaxDictionary: longTermTax!, taxableIncome: filingStatus.CurrentTaxableIncome)
+            filingStatus.TaxOnLTCapitalGain = GetCapitalGainTax(shortTermGL, termTaxDictionary: shortTermTax!, taxableIncome: filingStatus.CurrentTaxableIncome)
+            
+        }
+        else if(longTermGLIncluding1256 < 0 && shortTermGLIncluding1256 >= 0)
+        {
+            if(longTermGL + shortTermGL > 0)
+            {
+                filingStatus.TaxOnSTCapitalGain = GetCapitalGainTax(longTermGL + shortTermGL, termTaxDictionary: shortTermTax!, taxableIncome: filingStatus.CurrentTaxableIncome)
+            }
+            else
+            {
+                filingStatus.NetLoss = longTermGL + shortTermGL
+            }
+        }
+        else if(longTermGLIncluding1256 >= 0 && shortTermGLIncluding1256 < 0)
+        {
+            if(longTermGL + shortTermGL > 0)
+            {
+                filingStatus.TaxOnLTCapitalGain = GetCapitalGainTax(longTermGL + shortTermGL, termTaxDictionary: longTermTax!, taxableIncome: filingStatus.CurrentTaxableIncome)
+            }
+            else
+            {
+                filingStatus.NetLoss = longTermGL + shortTermGL
+            }
+        }
+        else if(longTermGLIncluding1256 < 0 && shortTermGLIncluding1256 < 0)
+        {
+            filingStatus.NetLoss = longTermGL + shortTermGL
+        }
+        
+        filingStatus.TaxOnLTSTCapitalGain = filingStatus.TaxOnLTCapitalGain + filingStatus.TaxOnSTCapitalGain
     }
     
-   // func GetCapitalGainTax(termGL Float, T
+    func GetCapitalGainTax(termGL: Double, termTaxDictionary : TaxBracket, taxableIncome : Double ) -> Double
+    {
+        var totalIncomeIncludiingCapitalGain : Double
+        var taxOnCapitalGain : Double = 0
+        var testDict : Dictionary<Double,Double> = Dictionary<Double,Double>()
+        
+        let cntTaxDictionary = termTaxDictionary.FederalTax.count
+        
+     //   let xx = termTaxDictionary.FederalTax as Dictionary<Double,Double>!
+        let maxTaxValue = termTaxDictionary.FederalTax.array[cntTaxDictionary - 1]
+        let maxTaxItem = termTaxDictionary.FederalTax.dictionary[maxTaxValue] //TODO - Double can't be value
+        
+        totalIncomeIncludiingCapitalGain = taxableIncome + termGL
+      
+        
+        for item in termTaxDictionary.FederalTax.array
+        {
+            if(item >= taxableIncome)
+            {
+                let x = totalIncomeIncludiingCapitalGain - item
+                
+                if(x < 0)
+                {
+                    let taxOnCapitalGain = taxOnCapitalGain + (termGL * termTaxDictionary.FederalTax.dictionary[item]!)
+                    break;
+                }
+                
+                else
+                {
+                    let taxOnCapitalGain = (item - taxableIncome) * termTaxDictionary.FederalTax.dictionary[item]!
+                    let termGL = x
+                    let taxableIncome = totalIncomeIncludiingCapitalGain
+                    continue
+                }
+            }
+            
+            else if (item == maxTaxItem)
+            {
+                let taxOnCapitalGain = taxOnCapitalGain + (termGL * maxTaxValue)
+            }
+        }
+        
+        return taxOnCapitalGain
+    }
 }
